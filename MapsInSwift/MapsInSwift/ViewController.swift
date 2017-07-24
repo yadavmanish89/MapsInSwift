b1a58bae034ced5db8f0c3478940067e54a4cbb4
Manish Yadav <yadavmanish89@gmail.com>
2017-07-24 19:47:55 -0400

    add nav controller

--- MapsInSwift/MapsInSwift/ViewController.swift
+++ MapsInSwift/MapsInSwift/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.titleView = self.searchBar
     }
 
     override func didReceiveMemoryWarning() {
