bd3326d35d4701742150356f2060aaa050f46e60
Manish Yadav <yadavmanish89@gmail.com>
2017-07-24 23:18:17 -0400

    remove unwanted string

--- MapsInSwift/MapsInSwift/AppDelegate.swift
+++ MapsInSwift/MapsInSwift/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GMSServices.provideAPIKey("AIzaSyBykzC0xOa5egGJfzf2nwdKpFuJDqSBYIY")
         GMSPlacesClient.provideAPIKey("AIzaSyBykzC0xOa5egGJfzf2nwdKpFuJDqSBYIY")
         return true
-    }MapsInSwift/MapsInSwift.xcodeproj/project.xcworkspace/xcuserdata/manish.xcuserdatad/UserInterfaceState.xcuserstate
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
